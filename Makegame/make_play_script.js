cb83a122e3b26b30081e3570c3c3b33f03f4de85
MathotaGamma <159739543+MathotaGamma@users.noreply.github.com>
2024-04-20 15:22:29 +0900

    Update make_play_script.js

--- Makegame/make_play_script.js
+++ Makegame/make_play_script.js
@@ -1,7 +1,15 @@
 const w = window.innerWidth;
 const h = window.innerHeight;
 
-l = localStorage.getItem('MakeGame:BlockList')
+let l = [];
+
+alert('play');
+
+l = localStorage.getItem('MakeGame:BlockList');
+
+const n_h = l.length;
+const n_w = l[0].length;
+const r = 30;
 
 alert(l.length);
 alert(l[0]);
@@ -11,5 +19,13 @@ function setup(){
 }
 
 function draw(){
-  background(255,0,0);
+  background(255);
+
+  fill(255);
+  for (let k = 0; k < n_h; k++){
+    for (let k0 = 0; k0 < n_w; k0++){
+      fill(255-100*l[k][k0]);
+      rect(k*r,k0*r,r,r);
+    }
+  }
 }
