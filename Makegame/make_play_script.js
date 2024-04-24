f475f0c70a97903fcf32e128b9800d3efc51a8cb
MathotaGamma <159739543+MathotaGamma@users.noreply.github.com>
2024-04-24 11:06:54 +0900

    Update make_play_script.js

--- Makegame/make_play_script.js
+++ Makegame/make_play_script.js
@@ -7,7 +7,19 @@ l = JSON.parse(localStorage.getItem('MakeGame:BlockList'));
 
 const n_h = l.length;
 const n_w = l[0].length;
-const r = 30;
+let r = 30;
+let left = 0;
+
+if(n_h/n_w > h/w){
+  //hがぴったり
+  r = h/n_h;
+  left = parseInt((w-r*n_w)/2);
+} else {
+  //wがぴったり
+  r = w/n_w;
+}
+
+
 
 
 
@@ -22,7 +34,7 @@ function draw(){
   for (let k = 0; k < n_h; k++){
     for (let k0 = 0; k0 < n_w; k0++){
       fill(255-100*l[k][k0]);
-      rect(k0*r,k*r,r,r);
+      rect(left+k0*r,k*r,r,r);
     }
   }
 }
