87b7a14dd3c5bcc5d3782fd2005d204f7569746c
MathotaGamma <159739543+MathotaGamma@users.noreply.github.com>
2024-04-25 15:56:57 +0900

    Update make_play_script.js

--- Makegame/make_play_script.js
+++ Makegame/make_play_script.js
@@ -48,6 +48,7 @@ try{
     }
   }
   N_n_k_k = N_n_k_k.split("").reverse();
+  alert(N_n_k_k);
   let k_k0 = 0;
   for(let k_k_h = 0; k_k_h < n_h; k_k_h++){
     for(let k_k_w = 0; k_k_w < n_w; k_k_w++){
